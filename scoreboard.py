71295af9c9397ef74f7179d895a3a262c037e2bf
kamil-dev <kamil.przylepa@gmail.com>
2020-11-25 11:47:19 +0100

    25.11.2020

--- scoreboard.py
+++ scoreboard.py
@@ -13,12 +13,16 @@ class Scoreboard:
         self.prep_score()
 
     def prep_score(self):
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1)
+
+        score_str = "{:,}".format(rounded_score)
+
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
 
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
+
 
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
