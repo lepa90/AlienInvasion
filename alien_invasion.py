040fd1ba5235e0f420940a9dcb4bd76708ff8c03
kamil-dev <kamil.przylepa@gmail.com>
2020-11-30 15:47:10 +0100

    30.11.2020

--- alien_invasion.py
+++ alien_invasion.py
@@ -83,6 +83,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
             self.aliens.empty()
             self.bullets.empty()
 
@@ -159,6 +160,8 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_flet()
             self.settings.incrase_speed()
+            self.stats.level += 1
+            self.sb.prep_level()
         if collisions:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
--- game_stats.py
+++ game_stats.py
@@ -7,8 +7,12 @@ class GameStats:
         self.high_score = 0
 
 
+
     def reset_stats(self):
         self.ship_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
+
+
 
 
